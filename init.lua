a656051933b76a0ed942cba76845834c27da507f
HybridDog <adresse_test@t-online.de>
2015-03-07 10:03:53 +0100

    :¬{

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 local clock_cbox = {
 	type = "fixed",
 	fixed = {
-		{ -8/32, -3/32, 14/32, 8/32, 3/32, 16/32 },
-		{ -7/32, -5/32, 14/32, 7/32, 5/32, 16/32 },
-		{ -6/32, -6/32, 14/32, 6/32, 6/32, 16/32 },
-		{ -5/32, -7/32, 14/32, 5/32, 7/32, 16/32 },
-		{ -3/32, -8/32, 14/32, 3/32, 8/32, 16/32 }
+		{ -1/4, -3/32, 7/16, 1/4, 3/32, .5 },
+		{ -7/32, -5/32, 7/16, 7/32, 5/32, .5 },
+		{ -3/16, -3/16, 7/16, 3/16, 3/16, .5 },
+		{ -5/32, -7/32, 7/16, 5/32, 7/32, .5 },
+		{ -3/32, -1/4, 7/16, 3/32, 1/4, .5 }
 	}
 }
 
 local clock_sbox = {
 	type = "fixed",
-	fixed = { -8/32, -8/32, 14/32, 8/32, 8/32, 16/32 }
+	fixed = { -1/4, -1/4, 7/16, 1/4, 1/4, .5 }
 }
 
 local materials = {"plastic", "wood"}
@@ -55,6 +55,35 @@ for i = 1,11 do
 		selection_box = clock_sbox,
 		groups = {snappy=3, not_in_creative_inventory=1},
 		drop = "realclocks:analog_clock_"..m.."_12",
+		on_punch = function(pos, node, puncher, pt)
+			minetest.chat_send_all("k…")
+			local dir = puncher:get_look_dir()
+			local dist = vector.new(dir)
+
+			local plpos = puncher:getpos()
+			plpos.y = plpos.y+1.625
+
+			if node.param2 == 0 then
+				local shpos = {x=pos.x, y=pos.y, z=pos.z+7/16}
+
+				dist.z = shpos.z-plpos.z
+				local m = dist.z/dir.z
+				dist.x = dist.x*m
+				dist.y = dist.y*m
+				local newp = vector.add(plpos, dist)
+				local tp = vector.subtract(newp, shpos)
+				local newtime = 0.25+math.acos(-tp.y/math.hypot(tp.x, tp.y))/(4*math.pi)
+				local oldtime = minetest.get_timeofday()
+				if oldtime < 0.25
+				or oldtime > 0.75
+				or oldtime > newtime then
+					newtime = (0.5+newtime)%1
+				end
+				minetest.chat_send_all("time "..newtime*24)
+				minetest.set_timeofday(newtime)
+			end
+			minetest.chat_send_all("it works again")
+		end,
 	})
 
 end

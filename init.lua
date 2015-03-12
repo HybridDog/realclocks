6f338cd531a9333d19dc5d4e01d1c573bf143de9
HybridDog <adresse_test@t-online.de>
2015-03-12 21:08:08 +0100

    just a bit mistake
    I thought I already deleted this

--- init.lua
+++ init.lua
@@ -16,7 +16,77 @@ local clock_sbox = {
 
 local materials = {"plastic", "wood"}
 
-for _,m in ipairs(materials) do
+local function punch_clock(pos, node, puncher, pt)
+	local dir = puncher:get_look_dir()
+	local dist = vector.new(dir)
+
+	local plpos = puncher:getpos()
+	plpos.y = plpos.y+1.625
+
+	local newtime,a,b,c,mpa,mpc
+	b = "y"
+	if node.param2 == 0 then
+		a = "x"
+		c = "z"
+	elseif node.param2 == 1 then
+		a = "z"
+		c = "x"
+		mpa = -1
+	elseif node.param2 == 2 then
+		a = "x"
+		c = "z"
+		mpc = -1
+		mpa = -1
+	elseif node.param2 == 3 then
+		a = "z"
+		c = "x"
+		mpc = -1
+	else
+		return
+	end
+
+	mpa = mpa or 1
+	mpc = mpc or 1
+	local shpos = {[a]=pos[a], [b]=pos[b], [c]=pos[c]+7/16*mpc}
+
+	dist[c] = shpos[c]-plpos[c]
+	local m = dist[c]/dir[c]
+	dist[a] = dist[a]*m
+	dist[b] = dist[b]*m
+	local newp = vector.add(plpos, dist)
+	local tp = vector.subtract(newp, shpos)
+	tp[a] = tp[a]*mpa
+	local tm = math.acos(-tp[b]/math.hypot(tp[a], tp[b]))/(2*math.pi)
+	newtime = 0.5+tm
+	if tp[a] > 0 then
+		newtime = 0.5-tm
+	end
+	newtime = newtime/2
+	local oldtime = minetest.get_timeofday()
+	if oldtime > 0.5 then
+		newtime = 0.5+newtime
+	end
+	if oldtime > newtime then
+		newtime = (0.5+newtime)%1
+	end
+
+	minetest.set_timeofday(newtime)
+
+	local nodename = node.name
+	nodename = string.sub(nodename, 1, -3)
+	if string.sub(nodename, -1) ~= "_" then
+		nodename = nodename.."_"
+	end
+	local readtime = newtime*24
+	nodename = nodename..math.ceil(readtime%12)
+	if node.name ~= nodename then
+		node.name = nodename
+		minetest.set_node(pos, node)
+	end
+	minetest.chat_send_all("time "..readtime)
+end
+
+for _,m in pairs(materials) do
 
 minetest.register_node("realclocks:analog_clock_"..m.."_12", {
 	drawtype = "mesh",
@@ -31,6 +101,7 @@ minetest.register_node("realclocks:analog_clock_"..m.."_12", {
 	collision_box = clock_cbox,
 	selection_box = clock_sbox,
 	groups = {snappy=3},
+	on_punch = punch_clock
 })
 
 minetest.register_craft({
@@ -55,35 +126,7 @@ for i = 1,11 do
 		selection_box = clock_sbox,
 		groups = {snappy=3, not_in_creative_inventory=1},
 		drop = "realclocks:analog_clock_"..m.."_12",
-		on_punch = function(pos, node, puncher, pt)
-			minetest.chat_send_all("k…")
-			local dir = puncher:get_look_dir()
-			local dist = vector.new(dir)
-
-			local plpos = puncher:getpos()
-			plpos.y = plpos.y+1.625
-
-			if node.param2 == 0 then
-				local shpos = {x=pos.x, y=pos.y, z=pos.z+7/16}
-
-				dist.z = shpos.z-plpos.z
-				local m = dist.z/dir.z
-				dist.x = dist.x*m
-				dist.y = dist.y*m
-				local newp = vector.add(plpos, dist)
-				local tp = vector.subtract(newp, shpos)
-				local newtime = 0.25+math.acos(-tp.y/math.hypot(tp.x, tp.y))/(4*math.pi)
-				local oldtime = minetest.get_timeofday()
-				if oldtime < 0.25
-				or oldtime > 0.75
-				or oldtime > newtime then
-					newtime = (0.5+newtime)%1
-				end
-				minetest.chat_send_all("time "..newtime*24)
-				minetest.set_timeofday(newtime)
-			end
-			minetest.chat_send_all("it works again")
-		end,
+		on_punch = punch_clock
 	})
 
 end
